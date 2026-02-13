4eb7f99a019cfd20a89e12cb9d5bc08eee79a5b3
kundangupta04 <kundankr1200@outlook.com>
2026-02-13 09:50:00 +0530

    updating footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link, NavLink } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div className="">
-    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-      <div className="">
-        <img src={assets.logo} className='mb-5 w-32' alt="" />
-        <p className='w-full md:w-2/3 text-gray-600'>At Forever, we bring you quality fashion designed for comfort, confidence, and everyday style. From timeless essentials to trending pieces, we are committed to delivering value, reliability, and a seamless shopping experience.</p>
-      </div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className="">
+          <Link to="/"><img src={assets.logo} className='mb-5 w-32' alt="" /></Link>
+          <p className='w-full md:w-2/3 text-gray-600'>At Forever, we bring you quality fashion designed for comfort, confidence, and everyday style. From timeless essentials to trending pieces, we are committed to delivering value, reliability, and a seamless shopping experience.</p>
+        </div>
 
-      <div className="">
-        <p className='text-xl font-medium mb-5'>COMPANY</p>
-        <ul className='flex flex-col gap-1 text-gray-600'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
-        </ul>
-      </div>
+        <div className="">
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <NavLink to="/"><li>Home</li></NavLink>
+            <NavLink to="/about"><li>About us</li></NavLink>
+            <NavLink to='/cart'><li>Delivery</li></NavLink>
+            <NavLink to="/about"><li>Privacy policy</li></NavLink>
+          </ul>
+        </div>
 
-      <div className="">
-        <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-        <ul className='flex flex-col gap-1 text-gray-600'>
+        <div className="">
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
             <li>+1-235-987-7563</li>
             <li>contact@foreveryou.com</li>
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
       <div className="">
-        <hr className='text-gray-300'/>
+        <hr className='text-gray-300' />
         <p className='py-5 text-sm text-center'>Copyright 2025 © forever.com - All Right Reserved</p>
       </div>
     </div>
